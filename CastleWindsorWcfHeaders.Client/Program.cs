745318fc463256257259605c63c8210c591273d0
Maciej Lis <mail@maciejlis.com>
2018-07-29 15:26:52 +0200

    Added whole project

---
+++ CastleWindsorWcfHeaders.Client/Program.cs
@@ -0,0 +1,69 @@
+﻿using Castle.Facilities.WcfIntegration;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using CastleWindsorWcfHeaders.Service;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Configuration;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleWindsorWcfHeaders.Client
+{
+    class Program
+    {
+        public static string ServiceUrl = "http://localhost:60260/Service1.svc";
+
+        public static WindsorContainer Container;
+        static void Main(string[] args)
+        {
+            Container = new WindsorContainer();
+            Container.AddFacility<WcfFacility>();
+            Container.Register(Component
+                .For<IService1>()
+                .AsWcfClient(
+                    WcfEndpoint.BoundTo(new BasicHttpBinding())
+                    .At(ServiceUrl)
+                    .AddExtensions(new LangCodeHeaderBehavior())
+                ));
+
+            var service1 = Container.Resolve<IService1>();
+
+            var result = service1.GetData(5);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+
+    internal class LangCodeHeaderBehavior : IEndpointBehavior
+    {
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters) { }
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher) { }
+        public void Validate(ServiceEndpoint endpoint) { }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            clientRuntime.MessageInspectors.Add(new LangCodeHeaderInspector());
+        }
+    }
+
+    internal class LangCodeHeaderInspector : IClientMessageInspector
+    {
+        public void AfterReceiveReply(ref Message reply, object correlationState) { }
+
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            var langCodeHeader = MessageHeader.CreateHeader("LangCode", String.Empty, "PL");
+            request.Headers.Add(langCodeHeader);
+
+            return null;
+        }
+    }
+}
---
+++ CastleWindsorWcfHeaders.Service/Service1.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CastleWindsorWcfHeaders.Service
+{
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            var langCode = OperationContext.Current.IncomingMessageHeaders.GetHeader<string>("LangCode", String.Empty);
+
+            return $"Value: {value}, LangCode: {langCode}";
+        }
+    }
+}
